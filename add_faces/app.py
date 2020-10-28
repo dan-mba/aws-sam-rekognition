4e8051a8bfaa2904e6614e69b33fea2d1f059d30
Dan <35927536+dan-mba@users.noreply.github.com>
2020-10-28 08:24:10 -0400

    add try logic

--- add_faces/app.py
+++ add_faces/app.py
@@ -15,26 +15,34 @@ def lambda_handler(event, context):
         }),
     }
 
-    body = base64.b64decode(event['body'])
-    
-    # Load face detection cascade
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    try:
+        body = base64.b64decode(event['body'])
 
-    # Load image
-    np_img = np.frombuffer(body, dtype=np.uint8)
-    img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+        # Load face detection cascade
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    # Convert into grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        # Load image
+        np_img = np.frombuffer(body, dtype=np.uint8)
+        img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
 
-    # Create return image
-    output_img = cv2.imencode('.png',img)[1]
-    output_img = base64.b64encode(output_img).decode('utf-8')
+        # Convert into grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        # Draw rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        # Create return image
+        output_img = cv2.imencode('.png',img)[1]
+        output_img = base64.b64encode(output_img).decode('utf-8')
+    except Exception as error:
+        return {
+            "statusCode": "200",
+            "body": json.dumps({
+                "message": str(error)
+            })
+        }
 
     return {
         "statusCode": 200,
