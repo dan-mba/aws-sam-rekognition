ffc3df340b669c339f62459dacabd4959c7f2885
Dan <35927536+dan-mba@users.noreply.github.com>
2020-11-03 08:27:26 -0500

    works with rekognition

--- add_faces/app.py
+++ add_faces/app.py
@@ -17,26 +17,27 @@ def lambda_handler(event, context):
     }
 
     try:
-        img_bytes = event['body']
-        img_decoded = base64.b64decode(img_bytes)
+        body = event['body']
+        img_decoded = base64.b64decode(body)
 
         # Load image
         np_img = np.frombuffer(img_decoded, dtype=np.uint8)
         img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+        height, width, _ = img.shape
 
         faces = client.detect_faces(
             Image={
-                'Bytes': img_bytes
+                'Bytes': img_decoded
             }
         )
 
-        return {
-            "statusCode": "200",
-            "body": json.dumps(faces)
-        }
+        # Draw rectangle around the faces
+        for face in faces['FaceDetails']:
+            x = int(round(face['BoundingBox']['Left'] * width))
+            y = int(round(face['BoundingBox']['Top'] * height))
+            w = int(round(face['BoundingBox']['Width'] * width))
+            h = int(round(face['BoundingBox']['Height'] * height))
 
-        # Draw rectangle around the faces TODO update for Rekognition
-        for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
         # Create return image
